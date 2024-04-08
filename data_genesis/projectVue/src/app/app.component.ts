3f5e17afffaacbb4e9e25c0a98a42db1653abb0c
thonyETU1883 <thonyrasolofondraibe1883@gmail.com>
2024-04-08 09:05:00 +0200

    authentification11

--- data_genesis/projectVue/src/app/app.component.ts
+++ data_genesis/projectVue/src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angularCrud';
+
+  constructor(private router: Router) {}
+
+  isLoginPage(): boolean {
+    return this.router.url === '/login' || this.router.url === '/signIn';
+  }
 }
